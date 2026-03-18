0969e64be81770f843c27336cedf0927490cbbcb
Nicholas Cocciemiglio <nicholascoccie@yahoo.com>
2026-03-17 23:36:18 -0500

    javascript practice

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const text=`<h2>Javascript Practice Below...Please dont bully me</h2>`;
 document.getElementById("js1").innerHTML=text;
 
@@ -15,3 +17,25 @@ document.getElementById("js3").innerHTML=sum1(3,5);
 
 const product1 = (a,b,c) => a*b*c;
 document.getElementById("js4").innerHTML=product1(3,5,8);
+
+const person = {
+    name:"John",
+    age: 30,
+    city:"New York"
+}
+
+let text2="";
+for(let x in person){
+    text2+=person[x] + " ";
+}
+document.getElementById("js5").innerHTML=text2;
+
+function Person(first, last, age, eye){
+    this.firstName=first;
+    this.lastName=last;
+    this.age=age;
+    this.eye=eye;
+}
+
+const myFriend=new Person("nick","coccie",23,"blue");
+document.getElementById("js6").innerHTML="My friend is " + myFriend.age + ".";
