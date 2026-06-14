1ee71ce53e29aaa945370149b45b1dd3f9ce2883
Nicholas Cocciemiglio <nicholascoccie@yahoo.com>
2026-06-14 00:13:46 -0500

    TypeScript practice 3

--- src/typescriptPractice.ts
+++ src/typescriptPractice.ts
@@ -5,3 +5,20 @@ function greet(name: string): string{
 }
 const test:string = greet("Nicholas.C first typescript test");
 document.getElementById("ts1")!.innerHTML = test;
+
+// Types
+let num12: number = 13;
+document.getElementById("ts2")!.innerHTML = String(num12);
+
+// Special types ?, .?, ??
+type Car = {
+    model: string;
+    year?: number;
+};
+
+const car: Car = {
+    model: "Mustang",
+    
+}
+
+document.getElementById("ts3")!.innerHTML = car?.year;
